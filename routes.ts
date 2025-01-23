df6f120ec0d4620cb677bcd3d245eb4d9306dbd2
Aaron <aaronbee1990@gmail.com>
2025-01-23 15:16:25 -0300

    começando autenticação om yup

--- routes.ts
+++ routes.ts
@@ -1,46 +1,33 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 
 import TaskController from "./src/controllers/TaskController";
 
 const taskController = new TaskController();
 const router = Router();
 
-
-{
-   router.get('/', (req:Request, res:Response) : any =>{
-      return res.json({get:'Home'})
-   })
-
-   //recuperar tarefa tal id
-   router.get('/task/:id',(req: Request, res: Response) : any =>{
-      taskController.getById(req, res);
-   })
-
-   //recuperar todas as tarefas
-   router.get('/task',(req: Request, res: Response) : any =>{
-      taskController.getAll(req, res);
-   })
-
-   //adicionar tarefa
-   router.post('/task', (req: Request, res: Response) : any =>{
-      const json =taskController.add(req,res);
-      
-   })
-
-   //editar tarefa por id
-   router.put('/task/:id',(req: Request, res: Response) : any =>{
-      taskController.update(req, res);
-   })
+const authMiddleware = (Req:Request, Res:Response, next: NextFunction) => {
+   if(Req.headers.authorization){
+      //Fazer verificações
+      next();
+   }else{
+      Res.json({error:'usuário não autenticado'});
+      Res.status(400);
+   }
 }
 
+
+//home
+router.get('/', (req:Request, res:Response) : any =>{return res.json({get:'Home'})})
+//recuperar tarefa tal id
+router.get('/task/:id',(req: Request, res: Response) : any =>{taskController.getById(req, res);})
+//recuperar todas as tarefas
+router.get('/task',(req: Request, res: Response) : any =>{taskController.getAll(req, res);})
+//adicionar tarefa
+router.post('/task', (req: Request, res: Response) : any =>{const json =taskController.add(req,res);})
+//editar tarefa por id
+router.put('/task/:id',(req: Request, res: Response) : any =>{taskController.update(req, res);})
 //deletar
-router.delete('/task/:id',(req: Request, res: Response) : any =>{
-   taskController.delete(req,res);
-})
-
-{
-
-}
+router.delete('/task/:id',(req: Request, res: Response) : any =>{taskController.delete(req,res);})
 
 
 export default router;
