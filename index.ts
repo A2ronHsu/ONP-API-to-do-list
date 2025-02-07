38546337eff758859ffde808f941529e690cf21a
Aaron <aaronbee1990@gmail.com>
2025-02-07 16:06:36 -0300

    commit with gitignore, starting jwt

--- index.ts
+++ index.ts
@@ -1,6 +1,22 @@
 import express, { urlencoded } from "express";
-import { Request, Response } from "express";
 import router from "./routes";
+import {v4 as uuidv4} from 'uuid';
+import * as jwt from 'jsonwebtoken';
+
+const payload : jwt.JwtPayload = {
+   userID : 'fdac2698-533f-4e75-b427-49185000b6f6',
+   name : 'Aaron',
+}
+
+const secret = 'teste-para-jwt';
+
+const option = {
+   expiresIn: 3600 
+}
+
+const token = jwt.sign(payload,secret,option);
+
+console.log(token)
 
 const server = express() ;
 
--- temp.ts
+++ temp.ts
@@ -1,5 +1,36 @@
-const arr = [1,2,3,4,5];
+function dumpObject(instance:any) {
+   // Dump instance properties and its prototype chain
+   console.log("=== Instance Properties ===");
+   console.log(instance);
 
-console.log(arr.slice(0,2).concat(arr.slice(3,5)));
+   let currentInstance = instance;
+   let depth = 0;
 
+   while (currentInstance) {
+       console.log(
+           `Prototype chain depth ${depth} (Class: ${currentInstance.constructor.name}):,
+           ${Object.getOwnPropertyNames(currentInstance)}`
+       );
+       currentInstance = Object.getPrototypeOf(currentInstance);
+       depth++;
+   }
 
+   // Dump class prototype chain
+   const classPrototype = Object.getPrototypeOf(instance.constructor);
+   console.log("\n=== Class Prototype ===");
+   console.log(classPrototype);
+
+   let currentClass = classPrototype;
+   depth = 0;
+
+   while (currentClass) {
+       console.log(
+          ` Class prototype chain depth ${depth} (Class: ${currentClass.constructor ? currentClass.constructor.name : 'null'}):,
+            ${Object.getOwnPropertyNames(currentClass)}`
+       );
+       currentClass = Object.getPrototypeOf(currentClass);
+       depth++;
+   }
+}
+
+dumpObject(new Date());
